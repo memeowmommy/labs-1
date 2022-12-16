b379e29c8112ef2a6cda85b5a816f5128b49d028
sasha <115340787+memeowmommy@users.noreply.github.com>
2022-12-16 19:32:17 +0200

    Add files via upload

---
+++ 2.1.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+	int a, b, c = 0;
+	cin >> a;
+	cin >> b;
+	cin >> c;
+
+	if ((a % b == 0) && (b > c))
+		cout << a / b + c;
+	else if ((a % b == 0) && (b > c))
+		cout << a / b - c;
+	else
+		cout << (a + b) * c;
+	system("pause");
+	return 0;
+
+
+}
