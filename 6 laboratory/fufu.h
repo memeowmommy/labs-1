c5406ecd5bb22b6c9f850248a4affba705926d96
sasha <115340787+memeowmommy@users.noreply.github.com>
2022-12-28 20:40:00 +0200

    Add files via upload

---
+++ 6 laboratory/fufu.h
@@ -0,0 +1,15 @@
+#pragma once
+
+namespace am
+{
+	void input(int s, int n, int mas[100][100]);
+	bool prime(int x);
+	bool columns(int x[100], int y[100], int n);
+	bool fl1(int s, int n, int mas[100][100]);
+	bool fl2(int s, int n, int mas[100][100]);
+	void sum_of_lines(int s, int n, int mas[100][100], int sum[100]);
+	void sort(int s, int n, int mas[100][100], int sum[100]);
+	void output(int s, int n, int mas[100][100]);
+}
+	
+	
